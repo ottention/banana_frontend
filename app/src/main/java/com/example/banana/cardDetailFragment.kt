aa0db6b6e4ef6a2c1895c0cc98918648b57651a9
arimkim <kal72222222@gmail.com>
2023-06-01 15:02:35 +0900

    [feat] test fragment and add fragment_card_detail

--- app/src/main/java/com/example/banana/cardDetailFragment.kt
+++ app/src/main/java/com/example/banana/cardDetailFragment.kt
@@ -1,14 +1,18 @@
 package com.example.banana
 
-import android.graphics.Canvas
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.example.banana.customView.Card
-import com.google.android.flexbox.AlignContent
 import com.google.android.flexbox.AlignItems
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
@@ -45,11 +49,11 @@ class cardDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        var wordList = listOf<String>("학생", "디자이너", "취준생", "박사", "에구")
-        val view = inflater.inflate(R.layout.fragment_card_detail, container, false)
-        val card = view.findViewById<Card>(R.id.detailed_card)
+        var wordList = listOf<String>("학생", "디자이너", "취준생", "으악", "에휴", "에구구")
 
+        val view = inflater.inflate(R.layout.fragment_card_detail, container, false)
         val flexBoxAdapter =  KeywordViewAdapter(activity!!.baseContext, wordList)
+
         FlexboxLayoutManager(this.context).apply {
             flexWrap = FlexWrap.WRAP
             flexDirection = FlexDirection.ROW
@@ -58,11 +62,88 @@ class cardDetailFragment : Fragment() {
         }
 
         view.findViewById<RecyclerView>(R.id.recycler_view).adapter = flexBoxAdapter
-
+        val canvas = view.findViewById<FrameLayout>(R.id.detailed_card)
+        drawImage(canvas)
+        drawText(canvas)
         return view
     }
 
-    fun draw(canvas : Card) {
+    fun drawImage(canvas : FrameLayout) {
+        val image = ImageView(context)
 
+        // 위치
+        val x = 100.0f
+        val y = 100.0f
+
+        // 크기
+        val width = 100
+        val height = 100
+
+        // 위치 설정
+        image.x = x
+        image.y = y
+
+        //크기 설정
+        var imageLayoutParams = LinearLayout.LayoutParams(width, height)
+        image.layoutParams = imageLayoutParams
+        image.setImageResource(R.drawable.test_image)
+
+//        Glide.with(context).load(foodImages[i]).into(image)
+        canvas.addView(image)
     }
+
+    fun drawText(canvas : FrameLayout) {
+
+        val text = TextView(context)
+        // 위치
+        val x = 100.0f
+        val y = 100.0f
+        // 크기
+        val width = 100
+        val height = 100
+        // 위치 설정
+        text.x = x
+        text.y = y
+
+        // 상세설정
+        text.setTextColor(Color.BLACK)
+        text.setTextSize(12.0f)
+
+        //크기 설정
+        var textLayoutParams = LinearLayout.LayoutParams(width, height)
+        text.layoutParams = textLayoutParams
+        text.text = "여기에 받아온 값 리스트에서 빼서 하나씩" // 근데 폰트 크기는 어떻게 해;;;
+        canvas.addView(text)
+    }
+
+    fun drawLink(canvas : FrameLayout) {
+        val text = TextView(context)
+        // 위치
+        val x = 100.0f
+        val y = 100.0f
+        // 크기
+        val width = 100
+        val height = 100
+        // 위치 설정
+        text.x = x
+        text.y = y
+
+        //크기 설정
+        var textLayoutParams = LinearLayout.LayoutParams(width, height)
+        text.layoutParams = textLayoutParams
+        text.text = "여기에 받아온 값 리스트에서 빼서 하나씩" // 근데 폰트 크기는 어떻게 해;;;
+        text.setTextColor(Color.BLACK)
+        text.setTextSize(12.0f)
+
+        // 링크설정
+        text.linksClickable = true
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(("text값")))
+        text.setOnClickListener {
+            startActivity(intent)
+        }
+
+        canvas.addView(text)
+    }
+
+
 }
--- app/src/main/java/com/example/banana/model/LoginKaKaoResponseModel.kt
+++ app/src/main/java/com/example/banana/model/LoginKaKaoResponseModel.kt
@@ -5,4 +5,4 @@ import com.google.gson.annotations.SerializedName
 data class LoginKaKaoResponseModel(
     @SerializedName("accessToken") var accessToken: String,
     @SerializedName("refreshToken") var refreshToken: String,
-    )
+)
