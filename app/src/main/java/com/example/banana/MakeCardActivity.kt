5d9291e121b9fd136e3c71a0d5b9bb4f2b57bc86
arimkim <kal72222222@gmail.com>
2023-06-21 20:51:48 +0900

    [feat] add expand twoTouch listner to icon

--- app/src/main/java/com/example/banana/MakeCardActivity.kt
+++ app/src/main/java/com/example/banana/MakeCardActivity.kt
@@ -1,17 +1,20 @@
 package com.example.banana
 
-import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
+import android.graphics.Matrix
+import android.graphics.PointF
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnClickListener
 import android.view.View.OnTouchListener
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
@@ -32,28 +35,144 @@ class MakeCardActivity : AppCompatActivity() {
     val REQ_GALLERY = 1 // 앨범 권한 처리
     lateinit var cardView : FrameLayout
 
+    // 제스쳐 감지
+    private var v : View? = null
+    private var matrix //기존 매트릭스
+            : Matrix? = null
+    private val savedMatrix //작업 후 이미지에 매핑할 매트릭스
+            : Matrix? = null
+    private var midPoint //두손가락 터치 시 중신 포인트
+            : PointF? = null
+    private var oldDistance //터치 시 두손가락 사이의 거리
+            = 0f
+
+    private var oldDegree = 0.0 // 두손가락의 각도
+
+    enum class TOUCH_MODE {
+        NONE,  // 터치 안했을 때
+        SINGLE,  // 한손가락 터치
+        MULTI //두손가락 터치
+    }
+    var touchMode : TOUCH_MODE? = null
+
+    inner class DragListner : OnTouchListener {
+
+        var moveX = 0f
+        var moveY = 0f
+
+        override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+            val action = event!!.action
+            val parentWidth = (v!!.parent as ViewGroup).width // 부모 View 의 Width
+            val parentHeight = (v.parent as ViewGroup).height // 부모 View 의 Height
+
+            when(event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    moveX = v!!.x - event.x
+                    moveY = v!!.y - event.y
+                    if(v is EditText) {
+                        v.showSoftInputOnFocus
+                    }
+                }
+
+                MotionEvent.ACTION_POINTER_DOWN -> {
+                    if(event.pointerCount == 2) {
+                        touchMode = MakeCardActivity.TOUCH_MODE.MULTI
+                        var fx = (event.getX(0) - event.getX(1)).toDouble()
+                        var fy = event.getY(0) - event.getY(1)
+                        val oldDistance = Math.sqrt(fx*fx + fy*fy).toFloat()
+
+
+                        if (oldDistance > 5f) {
+                            savedMatrix!!.set(matrix)
+
+                            val x: Float = (event.getX(0) - event.getX(1))/ 2
+                            val y: Float = (event.getY(0) - event.getY(1)) / 2
+                            val midPoint = PointF(x,y)
+
+                            val radian = Math.atan2(
+                                (event.y - midPoint!!.y).toDouble(),
+                                (event.x - midPoint!!.x).toDouble()
+                            )
+                            oldDegree = radian * 180 / Math.PI
+                        }
+                    }
+                }
+
+                MotionEvent.ACTION_MOVE -> {
+                    if(touchMode == MakeCardActivity.TOUCH_MODE.MULTI) {
+
+                        var fx = (event.getX(0) - event.getX(1)).toDouble()
+                        var fy = event.getY(0) - event.getY(1)
+                        val newDistance = Math.sqrt(fx*fx + fy*fy).toFloat()
+                        if (newDistance > 5f) {
+                            matrix!!.set(savedMatrix)
+                            val scale: Float = newDistance / oldDistance
+                            matrix!!.postScale(scale, scale, midPoint!!.x, midPoint!!.y)
+                            val nowRadian = Math.atan2(event!!.y.toDouble() - midPoint!!.y, event!!.x.toDouble() - midPoint!!.x)
+                            val nowDegress = nowRadian * 180 / Math.PI
+                            val degree = (nowDegress - oldDegree) as Float
+                            matrix!!.postRotate(degree, midPoint!!.x, midPoint!!.y)
+                            v!!.matrix.set(matrix)
+                        }
+
+                    }else {
+                        v!!.x = v.getX() + (event.getX()) - (v.getWidth()/2)
+                        v!!.y = v.getY() + (event.getY()) - (v.getHeight()/2)
+
+                    }
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    if(v.getX() < 0){
+                        v.x = 0f
+                    }else if((v.getX() + v.getWidth()) > parentWidth){
+                        v.x = (parentWidth - v.getWidth()).toFloat();
+                    }
+
+                    if(v.getY() < 0){
+                        v.y = 0f
+                    }else if((v.getY() + v.getHeight()) > parentHeight){
+                        v.y = (parentHeight - v.getHeight()).toFloat();
+
+                    }
+
+                }
+
+                MotionEvent.ACTION_POINTER_UP -> {
+                    touchMode = TOUCH_MODE.NONE
+                }
+
+            }
+
+            return true
+        }
+    }
     inner class addIconListner : View.OnClickListener {
+
         val listener : OnTouchListener = DragListner()
         override fun onClick(v: View?) {
 
-            val image = ImageView(v!!.context)
+                    val image = ImageView(v!!.context)
                     image.setImageDrawable(v.background)
                     // 위치 설정
                     image.x = 10f
                     image.y = 10f
                     var imageLayoutParams = LinearLayout.LayoutParams(100,100)
                     image.layoutParams = imageLayoutParams
+                    image.scaleType = ImageView.ScaleType.MATRIX
                     image.setOnTouchListener(listener)
                     cardView.addView(image)
             }
     }
+
 
     inner class addTextBoxListner(type: String) : View.OnClickListener {
         val type = type
         val listener : OnTouchListener = DragListner()
         override fun onClick(v: View?) {
 
-            val text = TextView(v!!.context)
+            val text = EditText(v!!.context)
+
             // 위치
             val x = 100.0f
             val y = 100.0f
@@ -85,7 +204,8 @@ class MakeCardActivity : AppCompatActivity() {
         val addIconListner : OnClickListener = addIconListner()
         val addTextBox1Lister : OnClickListener = addTextBoxListner("h1")
         val addTextBox2Lister : OnClickListener = addTextBoxListner("h2")
-
+        // 입력받는 방법을 관리하는 Manager객체를  요청하여 InputMethodmanager에 반환한다.
+        val imm = getSystemService(INPUT_METHOD_SERVICE);
         var color : Color? = null
 
         val templateBtn01 = findViewById<Button>(R.id.btn_template01)
@@ -190,6 +310,7 @@ class MakeCardActivity : AppCompatActivity() {
                     Glide.with(this).load(imageUri).into(image)
                     findViewById<FrameLayout>(R.id.card).addView(image)
                     image.setOnTouchListener(listener)
+                    image.scaleType = ImageView.ScaleType.MATRIX
                 }
             }
         }
@@ -225,48 +346,5 @@ class MakeCardActivity : AppCompatActivity() {
         intent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
         intent.type = "image/*"
         startActivityForResult(intent, REQ_GALLERY)
-    }
-}
-
-class DragListner : OnTouchListener {
-
-    var moveX = 0f
-    var moveY = 0f
-
-    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-
-        val parentWidth = (v!!.parent as ViewGroup).width // 부모 View 의 Width
-        val parentHeight = (v.parent as ViewGroup).height // 부모 View 의 Height
-
-        when(event?.action) {
-            MotionEvent.ACTION_DOWN -> {
-                moveX = v!!.x - event.x
-                moveY = v!!.y - event.y
-            }
-
-            MotionEvent.ACTION_MOVE -> {
-                v!!.x = v.getX() + (event.getX()) - (v.getWidth()/2)
-                v!!.y = v.getY() + (event.getY()) - (v.getHeight()/2)
-
-            }
-
-            MotionEvent.ACTION_UP -> {
-                if(v.getX() < 0){
-                    v.x = 0f
-                }else if((v.getX() + v.getWidth()) > parentWidth){
-                    v.x = (parentWidth - v.getWidth()).toFloat();
-                }
-
-                if(v.getY() < 0){
-                    v.y = 0f
-                }else if((v.getY() + v.getHeight()) > parentHeight){
-                    v.y = (parentHeight - v.getHeight()).toFloat();
-
-                }
-
-            }
-        }
-
-        return true
     }
 }
