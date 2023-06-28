77da19fd225c5ab0bf70591b323f63cf8f3e11ec
ShinKing <ssk7750@naver.com>
2023-06-28 19:55:03 +0900

    Feat. qr 통신은 성공, 이미지 띄우기 처리 중

--- app/src/main/java/com/example/banana/CreateQRActivity.kt
+++ app/src/main/java/com/example/banana/CreateQRActivity.kt
@@ -1,87 +1,88 @@
 package com.example.banana
 
 import android.content.Intent
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry.NoModelLoaderAvailableException
 import com.example.banana.data.ResponseGetQRCode
 import com.example.banana.retrofit.API
 import com.example.banana.retrofit.RetrofitInstance
-import com.google.zxing.BarcodeFormat
-import com.journeyapps.barcodescanner.BarcodeEncoder
+import okhttp3.internal.notify
 import retrofit2.Call
 import retrofit2.Response
-import java.io.InputStream
-import javax.security.auth.callback.Callback
 
 class CreateQRActivity : AppCompatActivity() {
 
 
     private lateinit var retAPI :API
 
+    lateinit var imageViewQrCode : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_qractivity)
 
         //API 호출
-        retAPI = RetrofitInstance.retrofitInstance().create(API::class.java)
+//        retAPI = RetrofitInstance.retrofitInstance().create(API::class.java)
 
+        imageViewQrCode = findViewById(R.id.imageViewQrCode)
         val btn_x = findViewById<ImageView>(R.id.btn_x)
-        //qr 생성
-//        try {
-//            val barcodeEncoder = BarcodeEncoder()
-//            val bitmap = barcodeEncoder.encodeBitmap("https://www.naver.com/", BarcodeFormat.QR_CODE, 400, 400)
-//            val imageViewQrCode: ImageView = findViewById<View>(R.id.imageViewQrCode) as ImageView
-//
-//            getQRCode()
-//
-//            imageViewQrCode.setImageBitmap(bitmap)
-//        } catch (e: Exception) {
-//        }
+
+
+        var imageUrl = intent.getStringExtra("QrUrl")
+        Log.d("url", imageUrl.toString())
+
+                            Glide.with(this@CreateQRActivity)
+                        .load(imageUrl)
+                        .into(imageViewQrCode)
+
+
+
 
         //뒤로가기 버튼
         btn_x.setOnClickListener{
             val intent = Intent(this, FragmentActivity::class.java)
             startActivity(intent)
         }
-        getQRCode()
+//        getQRCode()
 
     }
 
-    val accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjg3OTM3MjQ3LCJleHAiOjE2OTA1MjkyNDd9.aiKbUg52Uj0rSvQTumCd_pfvc_SOlk6C4xKcaN1tZbE"
-    private fun getQRCode() {
-        retAPI.getQRCode(accessToken).enqueue(object : retrofit2.Callback<String>{
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                if (response.isSuccessful) {
-
-
-
-                    var imageString = response.body()
-//                    var toBitmap = BitmapFactory.decodeByteArray(image,0,image!!.size)
-//                    var bitmap = BitmapFactory.decodeStream(imageString)
-//                    val imageBytes = Base64.decode(imageString,0)
-//                    val image = BitmapFactory.decodeByteArray(imageBytes,0,imageBytes.size)
-                    val imageViewQrCode: ImageView = findViewById<View>(R.id.imageViewQrCode) as ImageView
+//    val accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjg3OTM3MjQ3LCJleHAiOjE2OTA1MjkyNDd9.aiKbUg52Uj0rSvQTumCd_pfvc_SOlk6C4xKcaN1tZbE"
+//    private fun getQRCode() {
+//        retAPI.getQRCode(accessToken).enqueue(object : retrofit2.Callback<ResponseGetQRCode>{
+//            override fun onResponse(call: Call<ResponseGetQRCode>, response: Response<ResponseGetQRCode>) {
+//                if (response.isSuccessful) {
 //
-//                    imageViewQrCode.setImageBitmap(image)
-                    Glide.with(this@CreateQRActivity)
-                        .load(imageString)
-                        .into(imageViewQrCode)
-                    Log.d("getQr Response : ", "success")
-
-                } else {
-                    Log.d("getQr Response : ", "Code: ${response.code()} , Message: ${response.message()} , Success: ${response.isSuccessful}")
-                }
-            }
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                Log.d("getQr Response : ", "Fail 2")
-            }
-
-        })
-    }
+//
+//
+//                    var imageString = response.body()
+////                    var toBitmap = BitmapFactory.decodeByteArray(image,0,image!!.size)
+////                    var bitmap = BitmapFactory.decodeStream(imageString)
+////                    val imageBytes = Base64.decode(imageString,0)
+////                    val image = BitmapFactory.decodeByteArray(imageBytes,0,imageBytes.size)
+//                    val imageViewQrCode: ImageView = findViewById<View>(R.id.imageViewQrCode) as ImageView
+//
+////
+////                    imageViewQrCode.setImageBitmap(image)
+//                    Glide.with(this@CreateQRActivity)
+//                        .load(imageString)
+//                        .into(imageViewQrCode)
+//
+//                    Log.d("getQr Response : ", "success")
+//
+//                } else {
+//                    Log.d("getQr Response : ", "Code: ${response.code()} , Message: ${response.message()} , Success: ${response.isSuccessful}")
+//                }
+//            }
+//            override fun onFailure(call: Call<ResponseGetQRCode>, t: Throwable) {
+//                Log.d("getQr Response : ", "Fail 2")
+//            }
+//
+//        })
+//    }
 }
--- app/src/main/java/com/example/banana/retrofit/API.kt
+++ app/src/main/java/com/example/banana/retrofit/API.kt
@@ -1,5 +1,6 @@
 package com.example.banana.retrofit
 
+import android.widget.ImageView
 import com.example.banana.data.ResponseGetQRCode
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,9 +9,9 @@ import retrofit2.http.Header
 interface API {
 
     //qr 코드 가져오기
-    @GET("businessCard/3/qrcode/v3")
+    @GET("businessCard/1/qrcode/v4")
     fun getQRCode(
         @Header("Authorization") Authorization : String
-    ) : Call<String>
+    ) : Call<ResponseGetQRCode>
 
 }
