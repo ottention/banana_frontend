2a068a16ec49a7716375e27d0ea47dcb9328fc47
ShinKing <ssk7750@naver.com>
2023-07-25 12:57:02 +0900

    Feat. 카드 데이터 수정

--- app/src/main/java/com/example/banana/viewModel/AlarmViewModel.kt
+++ app/src/main/java/com/example/banana/viewModel/AlarmViewModel.kt
@@ -16,21 +16,7 @@ class AlarmViewModel: ViewModel() {
 //        showAlarm()
     }
 
-//    fun showAlarm() {
-//        _alarmList.value!!.add(AlarmData("23.03.03","000님이 방명록에 좋아요를 눌렀습니다."))
-//        _alarmList.value!!.add(AlarmData("23.03.02","000님이 방명록에 좋아요를 눌렀습니다."))
-//        _alarmList.value!!.add(AlarmData("23.03.01","000님이 방명록에 좋아요를 눌렀습니다."))
-//        _alarmList.value!!.add(AlarmData("23.02.03","000님이 방명록에 좋아요를 눌렀습니다."))
-//        _alarmList.value!!.add(AlarmData("23.01.03","000님이 방명록에 좋아요를 눌렀습니다."))
-//        _alarmList.value!!.add(AlarmData("23.01.08","000님이 방명록에 좋아요를 눌렀습니다."))
-//        _alarmList.value!!.add(AlarmData("23.01.07","000님이 방명록에 좋아요를 눌렀습니다."))
-//        _alarmList.value!!.add(AlarmData("23.01.06","000님이 방명록에 좋아요를 눌렀습니다."))
-//        _alarmList.value!!.add(AlarmData("23.01.04","000님이 방명록에 좋아요를 눌렀습니다."))
-//        _alarmList.value!!.add(AlarmData("23.01.01","000님이 방명록에 좋아요를 눌렀습니다."))
-//    }
 
-//    fun deleteAll() {
-//        _alarmList.value!!.clear()
-//    }
+
 
 }
